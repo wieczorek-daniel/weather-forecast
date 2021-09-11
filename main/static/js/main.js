aa380f981e2c2d656bc6a865efd9de879a7d754b
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2021-09-11 23:03:50 +0200

    input-validation

---
+++ main/static/js/main.js
@@ -0,0 +1,4 @@
+// Hide alert after 5 seconds
+setTimeout(function () {
+    document.querySelector('.alert').style.display = 'none';
+}, 5000);
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import urllib.request
+import urllib.parse
 import json
 from datetime import datetime
 from weather_forecast.settings import OPENWEATHERMAP_API_KEY
@@ -7,26 +8,31 @@ from weather_forecast.settings import OPENWEATHERMAP_API_KEY
 
 def index(request):
     if request.method == 'POST':
-        city = request.POST['city']
+        city = urllib.parse.quote_plus(request.POST['city'])
         request_url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&appid='+OPENWEATHERMAP_API_KEY
-        data_source = urllib.request.urlopen(request_url).read()
-        api_data = json.loads(data_source)
-
-        data = {
-            "country_code": api_data['sys']['country'],
-            "country_name": api_data['name'],
-            "latitude": api_data['coord']['lat'],
-            "longitude": api_data['coord']['lon'],
-            "temperature": str(api_data['main']['temp'])+f'\N{DEGREE SIGN}C',
-            "pressure": str(api_data['main']['pressure'])+'hPa',
-            "humidity": str(api_data['main']['humidity'])+'%',
-            "main_information": api_data['weather'][0]['main'],
-            "description": api_data['weather'][0]['description'],
-            "icon": api_data['weather'][0]['icon'],
-            "current_time": f"{datetime.utcfromtimestamp(api_data['dt']+api_data['timezone']):%Y-%m-%d %I:%M %p}",
-            "sunrise": f"{datetime.utcfromtimestamp(api_data['sys']['sunrise']+api_data['timezone']):%Y-%m-%d %I:%M %p}",
-            "sunset": f"{datetime.utcfromtimestamp(api_data['sys']['sunset']+api_data['timezone']):%Y-%m-%d %I:%M %p}",
-        }
+        try:
+            data_source = urllib.request.urlopen(request_url).read()
+            api_data = json.loads(data_source)
+            data = {
+                "country_code": api_data['sys']['country'],
+                "country_name": api_data['name'],
+                "latitude": api_data['coord']['lat'],
+                "longitude": api_data['coord']['lon'],
+                "temperature": str(api_data['main']['temp']) + f'\N{DEGREE SIGN}C',
+                "pressure": str(api_data['main']['pressure']) + 'hPa',
+                "humidity": str(api_data['main']['humidity']) + '%',
+                "main_information": api_data['weather'][0]['main'],
+                "description": api_data['weather'][0]['description'],
+                "icon": api_data['weather'][0]['icon'],
+                "current_time": f"{datetime.utcfromtimestamp(api_data['dt'] + api_data['timezone']):%Y-%m-%d %I:%M %p}",
+                "sunrise": f"{datetime.utcfromtimestamp(api_data['sys']['sunrise'] + api_data['timezone']):%Y-%m-%d %I:%M %p}",
+                "sunset": f"{datetime.utcfromtimestamp(api_data['sys']['sunset'] + api_data['timezone']):%Y-%m-%d %I:%M %p}",
+            }
+        except urllib.error.HTTPError:
+            data = {
+                "error_occurred": True,
+                "error_message": 'Can not find a city based on the entered text.',
+            }
     else:
         data = {}
 
