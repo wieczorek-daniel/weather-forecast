32593cb496903a3bf9149ca0f610b84f9497d9a1
Daniel Wieczorek <wieczorek.daniel@outlook.com>
2021-09-14 19:44:03 +0200

    heroku-deployment

--- weather_forecast/settings.py
+++ weather_forecast/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,10 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-STATICFILES_DIRS = (
-    BASE_DIR.joinpath('static'),
-)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
